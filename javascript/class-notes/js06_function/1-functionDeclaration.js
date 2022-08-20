984fef4a93bf5b911b41324cdcb04042278bc690
profnoah <noah@clarusway.com>
2022-08-20 12:00:44 +0300

    ornek1

--- javascript/class-notes/js06_function/1-functionDeclaration.js
+++ javascript/class-notes/js06_function/1-functionDeclaration.js
@@ -3,10 +3,22 @@
 // ? ===============================================
 
 // !------------------------------------------------
-// ! 1.YÖNTEM  : FUNCTION DECLARATION
+// ! 1.YONTEM  : FUNCTION DECLARATION
 // !------------------------------------------------
 
-console.log("****** FUNC DECLARATION *********");
+console.log("**** FUNC DECLARATION ******");
 
 //* ORNEK:
 //************************************************/
+yazdir();
+
+//! Fonksiyonun tanimlanmasi (declaration)
+function yazdir() {
+  console.log("merhaba");
+}
+
+yazdir(); //! invoke, call, cagirma
+yazdir();
+
+//* ORNEK2:
+//*************************************************/
