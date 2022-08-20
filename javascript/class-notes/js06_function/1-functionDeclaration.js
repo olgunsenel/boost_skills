f469292eb63decaf2d9fef4ea644c63dcf931804
profnoah <noah@clarusway.com>
2022-08-20 13:05:20 +0300

    func expression started

--- javascript/class-notes/js06_function/1-functionDeclaration.js
+++ javascript/class-notes/js06_function/1-functionDeclaration.js
@@ -22,3 +22,45 @@ yazdir();
 
 //* ORNEK2:
 //*************************************************/
+
+function selamla(ad, soyAd = "") {
+  console.log(`Merhaba ${ad} ${soyAd}`);
+}
+
+selamla("Can", "Yilmaz");
+selamla("Canan", "Ozturk");
+selamla("Ayse");
+selamla("John");
+
+//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+//! yerine default parametre atayabiliriz. Ornekteki lastName
+//! parametresi icin default değer olarak '' atanmistir.
+
+//* ORNEK3:
+//***************************************************/
+
+function yasHesapla(isim, dogumTarihi) {
+  // const sonuc = `${isim} in yasi ${2022 - dogumTarihi} dir.`;
+  // const sonuc = `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir.`;
+  // return sonuc;
+  return `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir.`;
+}
+
+const mesaj1 = yasHesapla("Elif Can", 1990);
+console.log(mesaj1);
+console.log(yasHesapla("Veli Canan", 1980));
+
+//* ORNEK4:
+//*****************************************************/
+
+function tekCift(sayi) {
+  return sayi % 2 ? `${sayi} TEKTIR` : `${sayi} CIFTTIR`;
+}
+
+console.log(tekCift(5));
+console.log(tekCift(2));
+
+//* ORNEK4:
+//*****************************************************/
+const n = +prompt("Bir sayi giriniz:");
+console.log(tekCift(n));
