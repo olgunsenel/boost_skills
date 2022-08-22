7fe37b204822fa62ac3236207cd9d7ac24dfa090
profnoah <noah@clarusway.com>
2022-08-22 20:44:08 +0300

    arrow

--- javascript/class-notes/js07_function-2/1-arrowFunction.js
+++ javascript/class-notes/js07_function-2/1-arrowFunction.js
@@ -8,5 +8,13 @@
 
 console.log("**** ARROW ******");
 
-//* ORNEK:
+//* ORNEK: Silindir Hacmi
 //************************************************/
+
+const r = Number(prompt("yaricap giriniz:"));
+const h = Number(prompt("yukseklik giriniz:"));
+
+const hacimHesapla = (r, h) => Math.PI * r * r * h;
+
+// console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toExponential()}`);
+console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
