26c93da6e30b95c0fcb0d1b01f475674895c3285
profnoah <noah@clarusway.com>
2022-08-22 22:36:18 +0300

    recursive

--- javascript/class-notes/js07_function-2/2-recursive.js
+++ javascript/class-notes/js07_function-2/2-recursive.js
@@ -12,15 +12,18 @@
 
 console.log("****** 2 - RECURSION ********");
 
-//? FIBONACCI terimleri:  0, 1, 1, 2, 3, 5, 8, 13, 21, ...
+//? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
+//! f(n) = f(n-1) + f(n-2)
 
 const fibo = (n) => {
-  
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fibo(n - 1) + fibo(n - 2);
+  }
 };
 
 const n = +prompt("n terimini giriniz:");
-if (n <= 0) {
-  console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
-} else {
-  console.log(`FIBO(${n})= ${fibo(n)}`);
-}
+console.log(`FIBO(${n})= ${fibo(n)}`);
