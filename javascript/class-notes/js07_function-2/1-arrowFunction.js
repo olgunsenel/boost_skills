ae4aa9209c768c796c2882eeff7dd17beb09332b
profnoah <noah@clarusway.com>
2022-08-22 20:58:25 +0300

    arrow

--- javascript/class-notes/js07_function-2/1-arrowFunction.js
+++ javascript/class-notes/js07_function-2/1-arrowFunction.js
@@ -18,3 +18,38 @@ const hacimHesapla = (r, h) => Math.PI * r * r * h;
 
 // console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toExponential()}`);
 console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
+
+//* ORNEK: Yas hesapla
+//************************************************/
+
+const tarih = Number(prompt("Dogum Tarihini giriniz:"));
+
+//! Arrow Func yontemi ile tanimlama
+const yasHesapla = (tarih) => {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+};
+console.log("YASINIZ:" + yasHesapla(tarih));
+
+//?-----------------------------------------
+
+// console.log("YASINIZ:" + yasHesapla2(tarih));
+//!Funct expression ve arrow func yontemlerinde
+//! Once fonks tanimlanmalidir sonra cagrilmalidir.
+//! Aksi takdirde hata alrsiniz.
+
+//! Func Expression yontemi ile tanimlama
+const yasHesapla2 = function (tarih) {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+};
+//?-----------------------------------------
+
+console.log("YASINIZ:" + yasHesapla3(tarih));
+
+//! Func Declaration yontemi ile tanimlama
+function yasHesapla3(tarih) {
+  const yas = new Date().getFullYear() - tarih;
+  return yas;
+}
+//?-----------------------------------------
