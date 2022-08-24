e1ce32fb486121e77122da26461456748c78049c
profnoah <noah@clarusway.com>
2022-08-24 23:12:27 +0300

    final

--- javascript/class-notes/js08_strings/stringMethods.js
+++ javascript/class-notes/js08_strings/stringMethods.js
@@ -128,3 +128,96 @@ console.log(word.lastIndexOf("BE")); //? -1
 //* ----------------------------------------------------------
 //* search()
 //* ----------------------------------------------------------
+
+const buyukHarf = /[A-Za-z]/;
+const nokta = /[.]/;
+const virgul = /[,]/;
+console.log(word.search(buyukHarf)); //? ilk buyuk harfin indeksi 0
+console.log(word.search(virgul)); //?  18
+console.log(word.search(nokta)); //? -1
+
+//* ----------------------------------------------------------
+//* startsWidth() , endsWith() - case sensitive
+//* ----------------------------------------------------------
+let word2 = "Sen gulunce guller acar gul pembe!";
+console.log(new String(word2));
+console.log(word2.startsWith("Sen")); //? true
+console.log(word2.startsWith("gulunce")); //? false
+console.log(word2.startsWith("gulunce", 4)); //? true
+console.log(word2.endsWith("!")); //? true
+console.log(word2.endsWith("acar", 23)); //? true
+
+//* ----------------------------------------------------------
+//*  replace(searchFor, replaceWith) --immutable
+//* ----------------------------------------------------------
+let oku = "Oku Baban gibi, saf olma, saf olma";
+// console.log(oku.replace("saf olma", "basarili ol"));
+oku = oku.replace("saf olma", "basarili ol");
+// oku = oku.replace(/saf olma/gi, "basarili ol");
+console.log(oku);
+
+oku = oku.replace(/BASARILI/gi, "Zengin");
+console.log(oku);
+
+//! NOT: replace metodu eger Regex ile aksi belirtilmiyorsa (g) sadece ilk buldugunu degistirir.
+
+//* ----------------------------------------------------------
+//*  replaceAll() --immutable
+//* ----------------------------------------------------------
+let degistir = "daglar daglar yol ver gecem..";
+degistir = degistir.replaceAll("daglar", "Dağlar");
+console.log(degistir);
+
+//?Regex de kullanilabilir.
+let degistir2 =
+  "No woman, no cry" +
+  " No woman, no cry, eh, yeah" +
+  " Little darling, don't shed no tears" +
+  " No woman, no cry, eh";
+
+console.log(degistir2.replaceAll(/no/gi, "Hayir"));
+
+//* ----------------------------------------------------------
+//*  slice(beginIndex[, endIndex])
+//*  substring(beginIndex[, endIndex])
+//*  substr (depreceated)
+//* ----------------------------------------------------------
+const veysel = "Uzun ince bir yoldayim yuruyorum gunduz gece..";
+
+const sliced = veysel.slice(33);
+console.log(sliced, typeof sliced); //? gunduz gece..
+
+console.log(veysel.slice(17, 30)); //? dayim yuruyor.
+console.log(veysel.slice(-10)); //? duz gece..
+console.log(veysel.slice(-23, -19)); //? yuru
+
+console.log(veysel.substring(17, 30)); //? dayim yuruyor
+//! negatif indeks substring ile kullanilamaz.
+console.log(veysel.substring(-10)); //? Uzun ince bir yoldayim yuruyorum gunduz gece..
+
+//* ----------------------------------------------------------
+//* split([sep [, limit] ])
+//* ----------------------------------------------------------
+const tarkan = "Gel gunduzle gece olalim";
+const splited = tarkan.split(" ");
+console.log(splited, typeof splited); //! Bosluklara göre ayirarak Array'e cevirdi.
+
+const chars = tarkan.split("");
+console.log(chars); //* null karakterine göre harfleri ayırarark bir char dizisi oluşturdu.
+
+//* ----------------------------------------------------------
+//* trim();
+//* ----------------------------------------------------------
+const ramazan = "    Hoş geldin ya şehri Ramazan     ";
+console.log(ramazan);
+console.log(ramazan.length); // 36
+console.log(ramazan.trim());
+console.log(ramazan.trim().length); //27
+
+//! ODEV1: tarkan string'indeki kelimelerin sayisini donduren
+//! bir fonksiyon yaziniz.
+
+//! ODEV2:
+//! ----------------------------------------------------------
+// https://www.youtube.com/watch?v=b7vfp5G4brE
+// https://youtu.be/b7vfp5G4brE
