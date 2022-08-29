159d23db9c313df77f431e61cd09f638f50d8b16
profnoah <noah@clarusway.com>
2022-08-29 23:22:05 +0300

    findIndex

--- javascript/class-notes/js09_arrays/arrays.js
+++ javascript/class-notes/js09_arrays/arrays.js
@@ -138,14 +138,20 @@ console.log(array1);
 //? ===========================================================
 //?  DIZI ERISIM METOTLARI
 //? ===========================================================
-const sayilar1 = [3, 5, 2, "2", "üc", 2, "bes", 5];
+const sayilar1 = [3, 5, 2, "2", "uc", 2, "3", "bes", "5"];
 
 //* includes()
 //*-----------------------------------------------------------
+console.log(sayilar1.includes(5)); //? true
+console.log(sayilar1.includes("5")); //? false
 
-//* indexOf(),  lastIndexOf();
+//* indexOf(), lastIndexOf();
 //*-----------------------------------------------------------
 //* ilk eslesen indeksi dondurur.
+
+console.log(sayilar1.indexOf(2)); //? 2
+console.log(sayilar1.lastIndexOf(2)); //? 5
+console.log(sayilar1.lastIndexOf(4)); //? -1
 
 //! Odev: prompt ile konsoldan bir sayi istenmeli (string veya number
 //! olarak) eger bu girilen sayi, dizi icerisinde bulunuyorsa indisi
@@ -156,24 +162,48 @@ const sayilar1 = [3, 5, 2, "2", "üc", 2, "bes", 5];
 //* join()
 //*-----------------------------------------------------------
 //? join, dizinin elamanlari birlestirip string hale cevirir.
+console.log(sayilar1.join(" "));
+console.log(sayilar1.join()); // 3,5...
+console.log(sayilar1);
 
 //* toString()
 //*-----------------------------------------------------------
 //? toString fonksiyonu sadece dizinin elemanlarinin aralarina
 //? (virgul) koyarak birlestirir ve string yapar.
+console.log(sayilar1.toString());
 
 //* slice()
 //*-----------------------------------------------------------
+const arabalar1 = ["BMW", "Mercedes", "Fiat", "Anadol"];
+const yeniArabalar = arabalar1.slice(2);
+console.log(yeniArabalar, arabalar1);
+
+const yeniArabalar1 = arabalar1.slice(1, 3);
+console.log(yeniArabalar1);
 
 //* concat()
 //*-----------------------------------------------------------
+const yazilar = ["Bugun", "hava", "cok", "guzel"];
+const numbersArr = [1, 2, 5, 7];
+const combinedArr = yazilar.concat(
+  true,
+  numbersArr,
+  ["aslinda", "cok", "sicak"],
+  [["x", "y"]]
+);
+console.log(combinedArr);
 
 //* every()
 //*-----------------------------------------------------------
-
 //? Tum diziyi itere eder ve aldigi callback fonksiyonuna gore
 //? test gerceklestirir.Tum elemanlar icin test basarili ise
 //? true aksi takdirde false deger dondurur.
+
+const yasArray = [18, 22, 78, 34, 78, 79, 15];
+const check = yasArray.every((yas) => yas >= 18);
+check
+  ? console.log("Dizideki herkesin yasi 18'den buyuktur")
+  : console.log("Dizide 18 yas alti var");
 
 //* some()
 //*-----------------------------------------------------------
@@ -181,14 +211,27 @@ const sayilar1 = [3, 5, 2, "2", "üc", 2, "bes", 5];
 //? En az bir eleman icin bile test basarili ise true aksi
 //? takdirde false deger dondurur.
 
-//* find()
+const buyuk80 = yasArray.some((y) => y >= 80);
+console.log(buyuk80); //? false
+
+//* find(), findLast()
 //*-----------------------------------------------------------
 //? Aldigi callback fonksiyonuna gore test gerceklestirir.
 //? Kosulu saglayan ilk dizi elemaninin dondurur.
 //? Eger hic bir eleman kosulu saglamazsa undefined dondurur.
+
+//?Ornek: Yasi 30 dan buyuk olan ilk elemani yazdirin
+const yasIlk30 = yasArray.find((yas) => yas >= 30);
+const yasSon30 = yasArray.findLast((yas) => yas >= 30);
+console.log(yasIlk30, yasSon30);
 
 //* findIndex()
 //*-----------------------------------------------------------
 //? Aldigi callback fonksiyonuna gore test gerceklestirir.
 //? Kosulu saglayan ilk dizi elemaninin indeksini dondurur.
 //? Eger hic bir eleman kosulu saglamazsa -1 dondurur.
+
+//?Ornek: Yasi 30 dan buyuk olan ilk elemanin indexini yazdirin
+
+const foundIndex = yasArray.findIndex((yas) => yas >= 30);
+console.log(foundIndex);
