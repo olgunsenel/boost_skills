9672f98bd2b55883cc262d019708ba401c0f8b7b
profnoah <noah@clarusway.com>
2022-08-29 20:35:04 +0300

    array

--- javascript/class-notes/js09_arrays/arrays.js
+++ javascript/class-notes/js09_arrays/arrays.js
@@ -6,3 +6,20 @@ console.log("***** ARRAYS ******");
 
 //* Dizi Tanimlama
 //* ---------------------------------------------------------
+
+// ! 1.Yöntem (Array Literal) - Tercih edilen yöntem
+const isimler = ["ahmet", "ismet", "can", "canan"];
+console.log(isimler);
+console.log(typeof isimler);
+console.log(isimler.length);
+
+// ! 2.Yöntem (Object Constructor)
+const diller = new Array("C++", "javascript", "Assembly", "Go", "Ruby");
+console.log(diller);
+
+const numbers = new Array(3, 2, 1);
+console.log(numbers);
+
+//! 10 elemanlik bos bir Array oluşturdu
+const numbers1 = new Array(10);
+console.log(numbers1);
