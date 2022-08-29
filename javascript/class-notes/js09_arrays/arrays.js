b470fe5debecea6fb15f3d66dcdae13a5bde6a03
profnoah <noah@clarusway.com>
2022-08-29 21:04:26 +0300

    write

--- javascript/class-notes/js09_arrays/arrays.js
+++ javascript/class-notes/js09_arrays/arrays.js
@@ -8,10 +8,13 @@ console.log("***** ARRAYS ******");
 //* ---------------------------------------------------------
 
 // ! 1.Yöntem (Array Literal) - Tercih edilen yöntem
-const isimler = ["ahmet", "ismet", "can", "canan"];
+const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
 console.log(isimler);
 console.log(typeof isimler);
 console.log(isimler.length);
+
+const bosDizi = [];
+console.log(bosDizi);
 
 // ! 2.Yöntem (Object Constructor)
 const diller = new Array("C++", "javascript", "Assembly", "Go", "Ruby");
@@ -23,3 +26,43 @@ console.log(numbers);
 //! 10 elemanlik bos bir Array oluşturdu
 const numbers1 = new Array(10);
 console.log(numbers1);
+
+// ! 3.Yöntem (Array.of())
+const veriler = Array.of(1, 2, 3);
+console.log(veriler);
+
+const veri = Array.of("Deneme");
+console.log(veri);
+
+//* Diziden Veri Okuma-Yazma (indisleme)
+//* ---------------------------------------------------------
+//!  Diziden veri okuma
+console.log(diller[1]);
+const go = diller[3];
+console.log(go);
+
+console.log(isimler[isimler.length - 1]);
+//? at()
+console.log(isimler.at(-1));
+
+//!  Diziye veri yazma
+// const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
+isimler[1] = "Saffet";
+console.log(isimler);
+
+//!Uncaught TypeError: Assignment to constant variable.
+// isimler = ["Ali", "Veli"];
+
+//? numbers dizisinin son elementinin degerini bir arttirin
+console.log(numbers);
+numbers[numbers.length - 1]++;
+console.log(numbers);
+
+const yaslar = [22, 44, 55];
+const kisiler = ["Ahmet", "Can", 2022, 2022 - 1990, true, yaslar];
+
+console.log(kisiler);
+
+//? kisiler icerisindeki yaslar dizisinde bulunan 55 yasini konsola yazdiriniz.
+
+//? kisiler icerisindeki yaslar dizisinde bulunan 44 yasini bir eksiltiniz.
