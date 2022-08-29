585f4c5e053bd78916fda623086df5776ab28746
profnoah <noah@clarusway.com>
2022-08-29 21:58:34 +0300

    access

--- javascript/class-notes/js09_arrays/arrays.js
+++ javascript/class-notes/js09_arrays/arrays.js
@@ -8,7 +8,7 @@ console.log("***** ARRAYS ******");
 //* ---------------------------------------------------------
 
 // ! 1.Yöntem (Array Literal) - Tercih edilen yöntem
-const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
+const isimler = ["ahmet", "ismet", "can", "canan", "matheww"];
 console.log(isimler);
 console.log(typeof isimler);
 console.log(isimler.length);
@@ -47,7 +47,7 @@ console.log(isimler.at(-1));
 
 //!  Diziye veri yazma
 // const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
-isimler[1] = "Saffet";
+isimler[1] = "saffet";
 console.log(isimler);
 
 //!Uncaught TypeError: Assignment to constant variable.
@@ -65,4 +65,76 @@ console.log(kisiler);
 
 //? kisiler icerisindeki yaslar dizisinde bulunan 55 yasini konsola yazdiriniz.
 
+const yas55 = kisiler[5][2];
+console.log(yas55);
+
 //? kisiler icerisindeki yaslar dizisinde bulunan 44 yasini bir eksiltiniz.
+kisiler[5][1]--; //?43
+console.log(kisiler);
+console.log(yaslar);
+
+//? ===========================================================
+//?  DIZIYI DEGISTIREN (MUTATOR) METOTLAR
+//? ===========================================================
+
+const arabalar = ["BMW", "Mercedes", "Fiat", "Anadol"];
+
+//** pop() son elemanı siler ve sildigi elemani dondurur.
+const silinen = arabalar.pop();
+console.log(arabalar, silinen);
+
+//* push() dizinin sonuna eleman ekler ve dizinin son eleman sayisini dondurur.
+
+const n = arabalar.push("Citroen");
+console.log(arabalar, n);
+
+//* unshift dizinin 0. indeksine eleman ekler ve dizinin son eleman sayisini dondurur.
+const n1 = arabalar.unshift("Audi");
+console.log(arabalar, n1);
+
+//* dizinin 0. indeks elemanini siler ve silenen elemani dondurur
+const audi = arabalar.shift();
+console.log(arabalar, audi);
+
+//* splice()
+//? 1.parametre: eklenecek indis numarasi
+//? 2.parametre: 0 ise araya ekleme, 1 ise uzerine yazma
+//? 3.parametre: yeni eklenecek veri
+arabalar.splice(1, 0, "Passat");
+console.log(arabalar);
+
+arabalar.splice(3, 1, "Honda", "Toyota");
+console.log(arabalar);
+
+//* Dizinin tamamini ters siraya cevirir.
+arabalar.reverse();
+console.log(arabalar);
+
+//* sort() Diziyi alfabetik olarak siralar.
+isimler.sort();
+console.log(isimler);
+
+//! Alfabetik siraladigi icin dogru sonuc cikmayabilir.
+const sayilar = [2, 111, 3, 22, 1, 11, 5, 7, 6];
+sayilar.sort();
+console.log(sayilar);
+
+//* sort metodu diziyi iterasyona ugratir ve parametre olark aldigi arrow
+//* fonksiyonunu dizinin her bir elemanina uygular. Bu sayede kucuk sayilar
+//* ile buyuk sayilari yer degistirerek siralama islemini gerceklestirir.
+sayilar.sort((a, b) => a - b);
+console.log(sayilar);
+
+//* fill()
+const array1 = [1, 2, 3, 4];
+array1.fill(0);
+console.log(array1);
+
+array1.fill(1, 2, 4);
+console.log(array1);
+array1.fill(-1, 1); //* 1. eleman ve sonrasini -1 yap
+console.log(array1);
+
+//? ===========================================================
+//?  DIZI ERISIM METOTLARI
+//? ===========================================================
