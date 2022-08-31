09e518b52648e63e0a1800c2b3d83c7dc482c833
profnoah <noah@clarusway.com>
2022-08-31 21:02:29 +0300

    for in

--- javascript/class-notes/js10_array-iteration/1-loops.js
+++ javascript/class-notes/js10_array-iteration/1-loops.js
@@ -26,10 +26,47 @@ console.log(pozitifler);
 console.log(dizi);
 
 //?-------------- ÖRNEK -------------------
+//? Dizideki notlarin ortalamasini hesaplayiniz.
+const notlar = [55, 77, 23, 89, 100];
+
+let toplam = 0;
+
+for (let i = 0; i < notlar.length; i++) {
+  toplam += notlar[i];
+}
+
+console.log("ORTALAMA:", toplam / notlar.length);
 
 //* ======================================================
 //*                   FOR-IN LOOP
 //* ======================================================
+
+//*-------------------------------------------------------
+//* SORU: Ogrenciler dizisinde ogrenci isimleri saklanmaktadir.
+//* ogrencileri aramamizi saglayacak ve aranilan ogrenciden
+//* ne kadar sayida bulunuldugunu  ana programa döndürecek bir
+//* fonksiyonu yaziniz. Eger aranilan isimde bir ogrenci yok ise
+//* fonksiyon "ogrenci bulunamadi" dondurulmelidir.
+//*--------------------------------------------------------
+
+const students = ["ahmet", "mehmet", "ismet", "ahmet", "can", "mehmet", "cem"];
+
+const findStudents = (arr, search) => {
+  let counter = 0;
+  for (let i in arr) {
+    if (search === arr[i]) {
+      counter++;
+    }
+  }
+  if (!counter) {
+    return `${search} can not be found`;
+  } else {
+    return `${search} found ${counter} times`;
+  }
+};
+
+const name = prompt("Please enter a name").toLowerCase();
+console.log(findStudents(students, name));
 
 //* ======================================================
 //*                   FOR-OF LOOP
