e2effc37958ed3fe74e374208c87dd5ff49dfa60
profnoah <noah@clarusway.com>
2022-08-31 20:32:11 +0300

    for

--- javascript/class-notes/js10_array-iteration/1-loops.js
+++ javascript/class-notes/js10_array-iteration/1-loops.js
@@ -4,24 +4,32 @@
 
 console.log("******* LOOPS IN ARRAYS ******");
 
+//?-------------- ÖRNEK -------------------
+const dizi = [-5, 15, 22, -4, 45, 78];
 
+const negatifler = [];
+const pozitifler = [];
 
+const dizelereAyir = (n) => {
+  for (let i = 0; i < n.length; i++) {
+    if (n[i] < 0) {
+      negatifler.push(n[i]);
+    } else {
+      pozitifler.push(n[i]);
+    }
+  }
+};
+dizelereAyir(dizi);
 
+console.log(negatifler);
+console.log(pozitifler);
+console.log(dizi);
 
-
-
-
+//?-------------- ÖRNEK -------------------
 
 //* ======================================================
 //*                   FOR-IN LOOP
 //* ======================================================
-
-
-
-
-
-
-
 
 //* ======================================================
 //*                   FOR-OF LOOP
