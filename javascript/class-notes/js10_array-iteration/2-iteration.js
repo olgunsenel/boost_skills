017fd778d0c43e44345636980c96d066ad99ba5f
profnoah <noah@clarusway.com>
2022-08-31 22:01:52 +0300

    forEach

--- javascript/class-notes/js10_array-iteration/2-iteration.js
+++ javascript/class-notes/js10_array-iteration/2-iteration.js
@@ -8,7 +8,20 @@ console.log("****** ITERATION **********");
 //?-------------- ÖRNEK -------------------
 //?  Dizideki herbir fiyati konsola bastiriniz.
 
-const fiyatlar = [100, 250, 50, 89];
+const prices = [100, 250, 50, 89];
+// prices.forEach((p) => console.log(p));
+
+//? ORNEK: prices dizisindekilerin toplamini konsola yazdiriniz */
+let sum = 0;
+prices.forEach((price) => (sum += price));
+console.log("SUM:", sum);
+
+//! NOT: forEach metodu void metottur.(Herhangi bir deger dondurmez)
+console.log(prices.forEach((price) => (sum += price))); //! undefined
+
+//?-------------- ÖRNEK -------------------
+//? prices dizisindeki her bir ara toplam degerini
+//? konsola bastiriniz. Ayrica her bir fiyata %10 zam yapiniz.
 
 //* ======================================================
 //*                       MAP METHOD
