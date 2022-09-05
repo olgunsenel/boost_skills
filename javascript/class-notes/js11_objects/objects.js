38f781d37a17d201106130bf19faae71bd2739b3
profnoah <noah@clarusway.com>
2022-09-05 22:02:29 +0300

    object method

--- javascript/class-notes/js11_objects/objects.js
+++ javascript/class-notes/js11_objects/objects.js
@@ -24,32 +24,99 @@ arabalar.lpg = true;
 console.log(arabalar);
 
 //!Read
-console.log(arabalar.lpg); //? .notation
-console.log("MODEL:", arabalar["model"]); //? Square bracket notation
+// console.log(arabalar.lpg); //? .notation
+// console.log("MODEL:", arabalar["model"]); //? Square bracket notation
 
-//? Square bracket yonteminin en buyuk avantaji key degerini
-//? degisken olarak kullanabilmemizdir.
-const key = "marka";
-console.log(arabalar[key]);
+// //? Square bracket yonteminin en buyuk avantaji key degerini
+// //? degisken olarak kullanabilmemizdir.
+// const key = "mark";
+// console.log(arabalar[key]);
 
-arabalar.motor = "1.6";
-console.log(arabalar);
+// arabalar.motor = "1.6";
+// console.log(arabalar);
+
+// //* ---------------------------------------------------------
+// //* 2- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
+// //* ---------------------------------------------------------
+
+// //? Object Constructure
+// function Personel(id, ad, maas) {
+//   this.id = id;
+//   this.ad = ad;
+//   this.maas = maas;
+//   // console.log(this); //! Personel objesine baglanmistir (binded)
+// }
+
+// const kisi1 = new Personel("1234567890", "Mustafa", 15000);
+// const kisi2 = new Personel("177567890", "Canan", 25000);
+// // console.log(kisi1);
+// console.log(kisi1.ad);
+// console.log(kisi2.maas);
+// console.log(kisi2["id"]);
+
+// //? Global alanda window objesini gosterir.
+// console.log(this);
+// window.alert("Merhaba");
 
 //* ---------------------------------------------------------
-//* 2- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
+//* 3- Object literal (En cok tercih edilen yontem)
 //* ---------------------------------------------------------
 
-//? Object Constructure
-function Personel(id, ad, maas) {
-  this.id = id;
-  this.ad = ad;
-  this.maas = maas;
-  // console.log(this);
-}
+const worker = {
+  name: "Can",
+  surname: "Canan",
+  age: 33,
+  job: "developer",
+  languages: ["C++", "Java", "Javacript", "Pyhton", "Go"],
+  salary: "140000",
+};
+console.log(worker);
 
-const kisi1 = new Personel("1234567890", "Mustafa", 15000);
-const kisi2 = new Personel("177567890", "Canan", 25000);
-// console.log(kisi1);
-console.log(kisi1.ad);
-console.log(kisi2.maas);
-console.log(kisi2["id"]);
+console.log("JOB:", worker.job);
+console.log("Lang:", worker["languages"]);
+
+console.log(worker.languages[2]);
+
+worker["languages"].forEach((l) => console.log(l));
+
+worker.dob = "1990";
+worker.email = "can@gmail.com";
+console.log(worker);
+
+// worker["salary"] = worker["salary"] * 1.1;
+worker["salary"] *= 1.1;
+console.log(worker);
+
+//* Object Copy
+const person = worker; //! Shallow (Sig - Sığ) Copying
+console.log("PERSON:", person);
+
+//! Object.create(), Object.assign(), spread (...), concat() , slice() -> Shallow Copying
+
+person.dob = 2000;
+console.log("PERSON:", person);
+
+//! Deep Copy
+let deepCopyOfWorker = JSON.parse(JSON.stringify(worker));
+// console.log(JSON.stringify(worker));
+
+deepCopyOfWorker.dob = "2022";
+console.log("DEEP:", deepCopyOfWorker);
+
+//* ======================================================
+//*              Object Metotlari
+//* ======================================================
+
+const personal = {
+  name: "Can",
+  surname: "Canan",
+  dob: "1990",
+  job: "developer",
+  salary: "140000",
+  drivingLicense: true,
+  calculateAge: function () {
+    return new Date().getFullYear() - this.dob;
+  },
+};
+
+console.log("DOB:", personal.calculateAge());
