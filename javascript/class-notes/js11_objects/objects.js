dbba666aa5b647975c5b6c609b4e7192a4c8f63d
profnoah <noah@clarusway.com>
2022-09-05 21:04:48 +0300

    object constructure

--- javascript/class-notes/js11_objects/objects.js
+++ javascript/class-notes/js11_objects/objects.js
@@ -10,3 +10,46 @@
 //? Herhangi bir veriyi erismek icin property (key) adi kullanilir.
 
 console.log("******* Objects *********");
+
+//! 3 farkli yontem ile Object olusturulabilir
+//* ---------------------------------------------------------
+//* 1- Object() class'ından new Operatörü ile
+//* ---------------------------------------------------------
+
+const arabalar = new Object();
+arabalar.marka = "BMW";
+arabalar.motor = "1.3";
+arabalar.model = 2022;
+arabalar.lpg = true;
+console.log(arabalar);
+
+//!Read
+console.log(arabalar.lpg); //? .notation
+console.log("MODEL:", arabalar["model"]); //? Square bracket notation
+
+//? Square bracket yonteminin en buyuk avantaji key degerini
+//? degisken olarak kullanabilmemizdir.
+const key = "marka";
+console.log(arabalar[key]);
+
+arabalar.motor = "1.6";
+console.log(arabalar);
+
+//* ---------------------------------------------------------
+//* 2- object constructor'i kullanarak (OOP ile ayrintilandirilacak)
+//* ---------------------------------------------------------
+
+//? Object Constructure
+function Personel(id, ad, maas) {
+  this.id = id;
+  this.ad = ad;
+  this.maas = maas;
+  // console.log(this);
+}
+
+const kisi1 = new Personel("1234567890", "Mustafa", 15000);
+const kisi2 = new Personel("177567890", "Canan", 25000);
+// console.log(kisi1);
+console.log(kisi1.ad);
+console.log(kisi2.maas);
+console.log(kisi2["id"]);
