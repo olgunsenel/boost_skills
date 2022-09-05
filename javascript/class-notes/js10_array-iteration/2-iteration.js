27e0d848b4302aa967cc1e82c9498b916cb139c0
profnoah <noah@clarusway.com>
2022-09-05 20:31:11 +0300

    reduce

--- javascript/class-notes/js10_array-iteration/2-iteration.js
+++ javascript/class-notes/js10_array-iteration/2-iteration.js
@@ -119,3 +119,20 @@ salaries
 //* ======================================================
 //*                 REDUCE METHOD
 //* ======================================================
+
+// const salaries = [5500, 8000, 6500, 9000, 10000, 15000, 25000];
+
+const sumOfSalaries = salaries.reduce((acc, val, i) => acc + val, 0);
+
+console.log("SUM:", sumOfSalaries);
+
+//? Ornek: Bir Firma, 9000 TL den az olan maaşlara %10 zam yapmak istiyor
+//? ve zam yapılan bu kişilere toplam kaç TL ödeneceğini bilmek istiyor.
+//? İlgili programı yazınız.
+
+const sumOfRaisedSalaries = salaries
+  .filter((sal) => sal <= 9000)
+  .map((sal) => Math.trunc(sal * 1.1))
+  .reduce((acc, salary) => acc + salary, 0);
+
+console.log("Sum Of Raised Salaries:", sumOfRaisedSalaries);
