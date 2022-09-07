fa23df1e8dbe7a59e7c3d9f82e964750e4d616a4
profnoah <noah@clarusway.com>
2022-09-07 21:03:45 +0300

    map

--- javascript/class-notes/js11_objects/objects.js
+++ javascript/class-notes/js11_objects/objects.js
@@ -228,3 +228,28 @@ Object.values(people)
 //********************************************************
 //* JSON => Javascript Object Notation
 //********************************************************
+const team = [
+  { name: "Josh", surname: "Adams", job: "developer", age: 30 },
+  { name: "Mary", surname: "Bary", job: "tester", age: 22 },
+  { name: "Hazel", surname: "Nut", job: "developer", age: 20 },
+]; //* JSON
+
+console.log(team);
+console.log(team[2]);
+
+//* Ornek1: team dizisindeki job'lari tek tek yazdiriniz.
+team.forEach((person) => console.log(person.job));
+
+//* Ornek2: age'leri bir artirarak yeni bir diziye saklayiniz.
+const ages = team.map((p) => p.age + 1);
+
+//* Ornek3: name ve surname'leri birlestirip buyuk harfe ceviren ve
+//* bunu fullName key'i olarak saklayan, ayni zamanda age degerlerini 5
+//* arttirarak age key'ine saklayan ve olusan diziyi donduren kodu yazınız.
+
+const teamFullName = team.map((p) => ({
+  fullName: p.name.toUpperCase() + " " + p.surname.toUpperCase(),
+  age: p.age + 5,
+}));
+
+console.log(teamFullName);
