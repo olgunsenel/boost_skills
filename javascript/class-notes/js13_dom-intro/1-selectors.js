675bc34d5d2a2fa7fd580d7b9cbec54f51d00d27
profnoah <noah@clarusway.com>
2022-09-10 12:02:57 +0300

    getElemetnsById

--- javascript/class-notes/js13_dom-intro/1-selectors.js
+++ javascript/class-notes/js13_dom-intro/1-selectors.js
@@ -6,3 +6,22 @@ console.log("***** SELECTORS *****");
 
 //* EXAMPLE-1
 //*-------------------------------------------
+
+const h1 = document.getElementById("header");
+console.log(h1);
+h1.style.color = "white";
+h1.style.background = "red";
+
+//* EXAMPLE-2
+//*-------------------------------------------
+
+const myInput = document.getElementById("input");
+const button = document.getElementById("btn");
+button.style.background = "black";
+button.style.color = "white";
+button.style.width = "7rem";
+button.style.border = "none";
+button.style.borderRadius = "10px";
+button.style.padding = "1rem";
+button.style.fontSize = "1.1rem";
+myInput.style.padding = "1rem";
