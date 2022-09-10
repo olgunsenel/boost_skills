636a847a0cf3ff64ad692179a4bbac0d66b2f1bb
profnoah <noah@clarusway.com>
2022-09-10 12:55:09 +0300

    querySelector

--- javascript/class-notes/js13_dom-intro/1-selectors.js
+++ javascript/class-notes/js13_dom-intro/1-selectors.js
@@ -73,3 +73,32 @@ myListArray.forEach((item) => console.log(item.innerText));
 //* ========================================
 //*              QUERYSELECTOR()
 //* ========================================
+
+//? 1- queryselector() etiket secebilir.
+const myLi = document.querySelector("li");
+console.log(myLi.innerText);
+myLi.style.color = "yellowgreen";
+
+//? 2- queryselector() class secebilir.
+
+const itemList = document.querySelector(".item-list");
+itemList.style.background = "grey";
+
+//? 3- queryselector() id secebilir.
+const input = document.querySelector("#input");
+console.log(input.value);
+
+document.querySelector("#btn").style.cursor = "pointer";
+
+//? 4- queryselector() ile CSS deki gibi secim yapilabilir.
+
+const itemH2 = document.querySelector(".item-list h2");
+itemH2.style.color = "purple";
+
+const myBtn = document.querySelector("input[type='button']");
+console.log(myBtn);
+
+
+//* ========================================
+//*              QUERYSELECTORALL()
+//* ========================================
