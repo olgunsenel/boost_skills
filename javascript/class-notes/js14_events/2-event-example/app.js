a1dc8c50f1a6b53e992eeb90e7f551173e295e60
profnoah <noah@clarusway.com>
2022-09-12 23:05:24 +0300

    todo finished

--- javascript/class-notes/js14_events/2-event-example/app.js
+++ javascript/class-notes/js14_events/2-event-example/app.js
@@ -27,6 +27,26 @@ deleteBtn.addEventListener("click", () => {
     : alert("There is no item to delete");
 });
 
+//? enter key and  del key event handler
+langInput.addEventListener("keydown", (event) => {
+  // console.log(event);
+  // console.log(event.target);
+  // console.log(event.keyCode);
+  // console.log(event.code);
+
+  if (event.keyCode === 13) {
+    addBtn.click();
+  }
+
+  // if (event.code === "Delete") {
+  //   deleteBtn.click();
+  // }
+
+  if (event.keyCode === 46) {
+    deleteBtn.click();
+  }
+});
+
 //? onload event handler
 window.addEventListener("load", () => {
   langInput.focus();
