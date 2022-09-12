2d88b33939d989bbc87e332a7e79712b215a1490
profnoah <noah@clarusway.com>
2022-09-12 21:00:31 +0300

    addEventListener

--- javascript/class-notes/js14_events/1-events-intro/events.js
+++ javascript/class-notes/js14_events/1-events-intro/events.js
@@ -7,9 +7,38 @@ console.log("****** EVENTS *******");
 //* EXAMPLE-1 (Mouse Over, Mouse Out )
 //* -------------------------------------------------------
 
+const h1 = document.querySelector("#header h1");
 
+//? 3 .method
+h1.onmouseover = function () {
+  h1.style.color = "red";
+  h1.style.fontWeight = "300";
+};
 
+h1.onmouseout = () => {
+  h1.style.color = "black";
+  h1.style.fontWeight = "900";
+};
 
+//? 4.method (addEventListener)
+h1.addEventListener("click", () => {
+  alert("H1 pressed");
+});
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
+
+document.getElementById("btn").addEventListener("click", function () {
+  const input = document.querySelector("#input");
+  // if (!input.value) {
+  //   alert("Please enter an item");
+  // } else {
+  //   alert(`${input.value} entered`);
+  // }
+
+  !input.value
+    ? alert("Please enter an item")
+    : alert(`${input.value} entered`);
+
+  input.value = "";
+});
