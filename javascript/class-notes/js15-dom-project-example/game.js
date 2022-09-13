7beac69be6592845b42b5672b230abb03f44d4be
profnoah <noah@clarusway.com>
2022-09-13 21:01:51 +0300

    v2

--- javascript/class-notes/js15-dom-project-example/game.js
+++ javascript/class-notes/js15-dom-project-example/game.js
@@ -4,26 +4,40 @@
 //*======================================
 
 //? 1-100 arasinda rasgele bir sayi tut.
-
 const randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
+
+//? Variables
+let score = 10;
+let topScore = 0;
 
 //* CheckBtn basildiginda kontrolleri yap
 document.querySelector(".check-btn").addEventListener("click", () => {
   const guessInput = Number(document.querySelector(".guess-input").value);
   const msg = document.querySelector(".msg");
-  //? eger input girilmediyse Kullaniciya uyari ver.
+  const body = document.querySelector("body");
 
+  //? eger input girilmediyse Kullaniciya uyari ver.
   if (!guessInput) {
     msg.innerText = "Please enter a number";
     //! eger rasgele == input.value
   } else if (randomNumber === guessInput) {
-    msg.innerText = "Congrats You Win";
+    msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
+    body.className = "bg-success";
+    if (score > topScore) {
+      topScore = score;
+      document.querySelector(".top-score").textContent = topScore;
+    }
+    document.querySelector(".secret-number").textContent = randomNumber;
+  } else {
+    score--;
+
+    guessInput > randomNumber
+      ? (msg.innerText = "DECREASE")
+      : (msg.innerText = "INCREASE");
   }
 });
 
-//? tebrikler bildiniz.
-//? background = green
 //? eger score > topScore
 //?     topScore = score
 //? secret_number = gorunur.
