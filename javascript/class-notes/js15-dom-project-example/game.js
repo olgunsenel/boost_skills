30f6356ea30670004851a1e144bb35f06ee87c04
profnoah <noah@clarusway.com>
2022-09-13 20:40:55 +0300

    v1

--- javascript/class-notes/js15-dom-project-example/game.js
+++ javascript/class-notes/js15-dom-project-example/game.js
@@ -2,3 +2,40 @@
 //*  GUESS MY NUMBER
 //*  Game Logic
 //*======================================
+
+//? 1-100 arasinda rasgele bir sayi tut.
+
+const randomNumber = Math.round(Math.random() * 100);
+console.log(randomNumber);
+
+//* CheckBtn basildiginda kontrolleri yap
+document.querySelector(".check-btn").addEventListener("click", () => {
+  const guessInput = Number(document.querySelector(".guess-input").value);
+  const msg = document.querySelector(".msg");
+  //? eger input girilmediyse Kullaniciya uyari ver.
+
+  if (!guessInput) {
+    msg.innerText = "Please enter a number";
+    //! eger rasgele == input.value
+  } else if (randomNumber === guessInput) {
+    msg.innerText = "Congrats You Win";
+  }
+});
+
+//? tebrikler bildiniz.
+//? background = green
+//? eger score > topScore
+//?     topScore = score
+//? secret_number = gorunur.
+
+//! değilse
+//! eger score > 0
+//!   score = score -1
+//?   eğer rasgele < input.value
+//?     AZALT
+//?   degilse
+//?     ARTTIR
+//! degise
+//? Uzgunuz kaybetiniz.
+
+//* againBtn basildiginda kontrolleri yap
