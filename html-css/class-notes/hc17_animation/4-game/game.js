f3f02c6dcdc08b1f4ef62f37fa58df69b46548fb
profnoah <noah@clarusway.com>
2022-09-15 23:02:52 +0300

    enter

---
+++ html-css/class-notes/hc17_animation/4-game/game.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body");
+const car = document.querySelector(".car");
+const ground = document.querySelector(".ground");
+
+body.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    if (!ground.classList.contains("right")) {
+      ground.classList.add("right");
+      car.classList.add("pop-up");
+    } else {
+      ground.classList.remove("right");
+      car.classList.remove("pop-up");
+    }
+  }
+});
