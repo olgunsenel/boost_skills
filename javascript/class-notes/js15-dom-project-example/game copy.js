a56489ddf5898cd2939ce70a9577b1cae9b61f8e
fume <olgunsenel@gmail.com>
2022-09-15 19:06:19 +0200

    game study

--- javascript/class-notes/js15-dom-project-example/game copy.js
+++ javascript/class-notes/js15-dom-project-example/game copy.js
@@ -3,19 +3,3 @@
 //*  Game Logic
 //*======================================
 
-
-const randomNumber = Math.round(Math.random() * 100);
-console.log(randomNumber);
-
-document.querySelector(`.check-btn`).addEventListener(`click`, () => {
-    const guessInput = Number(document.querySelector(`.guess-input`).value);
-    const msg = document.querySelector(`.msg`);
-    const body = document.querySelector(`body`);
-});
-
-if (!guessInput) {
-    msg.innerText = `Please enter a number`;
-} else if (randomNumber === guessInput) {
-    msg.innerHTML = `Congratulations! You WIN!`;
-    body.className = `bg-success`;
-}
