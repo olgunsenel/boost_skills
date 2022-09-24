3d92b9ed3781b40564fd8cd1f721fa0ecd8b1f4b
profnoah <noah@clarusway.com>
2022-09-24 14:22:03 +0300

    countires finished

--- javascript/class-notes/js19_async2/flag-app/app.js
+++ javascript/class-notes/js19_async2/flag-app/app.js
@@ -27,18 +27,41 @@ const renderError = () => {
 const renderCountries = (data) => {
   console.log(data);
   const countryDiv = document.querySelector(".countries");
+  const {
+    capital,
+    currencies,
+    flags: { svg },
+    languages,
+    name: { common },
+    region,
+  } = data[0];
 
-  countryDiv.innerHTML = `
-    <div class="card" style="width: 18rem;">
-      <img src="..." class="card-img-top" alt="...">
+  console.log(Object.values(languages));
+  console.log(Object.values(currencies)[0].name);
+  console.log(Object.values(currencies)[0].symbol);
+
+  countryDiv.innerHTML += `
+    <div class="card mx-auto m-3 shadow-lg" style="width: 18rem;">
+      <img src="${svg}" class="card-img-top" alt="...">
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">${common}</h5>
+        <p class="card-text">${region}</p>
       </div>
       <ul class="list-group list-group-flush">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">
+          <i class="fas fa-lg fa-landmark"></i> ${capital}
+        </li>
+        <li class="list-group-item">
+          <i class="fas fa-lg fa-comments"></i> ${Object.values(languages)}
+        </li>
+        <li class="list-group-item">
+          <i class="fas fa-lg fa-money-bill-wave"></i> ${
+            Object.values(currencies)[0].name
+          },
+
+          ${Object.values(currencies)[0].symbol},
+
+        </li>
       </ul>
       <div class="card-body">
         <a href="#" class="card-link">Card link</a>
@@ -51,3 +74,5 @@ const renderCountries = (data) => {
 };
 
 fetchCountryByName("turkey");
+fetchCountryByName("russia");
+fetchCountryByName("south africa");
