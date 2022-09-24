ce39f7721a4a4000a1077e2c7d89d117133b5ba6
profnoah <noah@clarusway.com>
2022-09-24 12:49:11 +0300

    card

--- javascript/class-notes/js19_async2/async-await/async-await.js
+++ javascript/class-notes/js19_async2/async-await/async-await.js
@@ -20,12 +20,40 @@
 
 const getNews = async function () {
   const API_KEY = "55e3f1fdc1c94581b6b11537c9b8f8bf";
-
   const url =
     "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
 
-  const res = await fetch(url);
-  console.log(res);
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`Something went wrong: ${res.status} `);
+    }
+    const data = await res.json();
+    // console.log(data.articles);
+    renderNews(data.articles);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-getNews();
+const renderNews = (news) => {
+  const newsList = document.getElementById("news-list");
+
+  news.forEach((item) => {
+    const { title, description, urlToImage, url } = item; //! dest
+    newsList.innerHTML += `
+    <div class="col-md-6 col-lg-4 col-xl-3">
+      <div class="card">
+        <img src="${urlToImage}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${description}</p>
+          <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+};
+
+window.addEventListener("load", getNews);
