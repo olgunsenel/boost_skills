a692abfe1d9eb04cbaba8f215adc1ce166784dfe
profnoah <noah@clarusway.com>
2022-09-24 13:42:27 +0300

    card

--- javascript/class-notes/js19_async2/flag-app/app.js
+++ javascript/class-notes/js19_async2/flag-app/app.js
@@ -1,3 +1,53 @@
 //*=========================================================
 //*                     FLAG-APP
 //*=========================================================
+
+const fetchCountryByName = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        renderError(`Something went wrong: ${res.status}`);
+        throw new Error();
+      }
+      return res.json();
+    })
+    .then((data) => renderCountries(data))
+    .catch((err) => console.log(err));
+};
+
+const renderError = () => {
+  const countryDiv = document.querySelector(".countries");
+  countryDiv.innerHTML += `
+    <h2>Countries can not fetched</h2>
+    <img src="./img/404.png" alt="" />
+  `;
+};
+
+const renderCountries = (data) => {
+  console.log(data);
+  const countryDiv = document.querySelector(".countries");
+
+  countryDiv.innerHTML = `
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+      </ul>
+      <div class="card-body">
+        <a href="#" class="card-link">Card link</a>
+        <a href="#" class="card-link">Another link</a>
+      </div>
+    </div>
+
+
+  `;
+};
+
+fetchCountryByName("turkey");
