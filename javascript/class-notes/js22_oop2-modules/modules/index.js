f57f6d99375d1829917d74efbf5d3c5a1e099523
fume <olgunsenel@gmail.com>
2022-10-02 22:58:34 +0200

    module

--- javascript/class-notes/js22_oop2-modules/modules/index.js
+++ javascript/class-notes/js22_oop2-modules/modules/index.js
@@ -1,6 +1,36 @@
 console.log("JS Modules is running");
 
+<<<<<<< HEAD
 
 import karesi, {WHW, dec, inc, sum, counter} from `./mymodule.js`;
 
 console.log(`WHW : >>, WHW`);
+=======
+import karesi, {
+  WHW as W,
+  WORKING_HOURS_WEEK,
+  dec,
+  inc,
+  sum,
+  counter,
+} from "./mymodule.js";
+import ugurla, { selam } from "./module2.js";
+
+// import { default as karesi, WHW, WORKING_HOURS_WEEK, dec, inc, sum, counter } from './mymodule.js';
+
+// console.log("W :>> ", W);
+// console.log("WORKING_HOURS_WEEK :>> ", WORKING_HOURS_WEEK);
+// console.log("karesi(4) :>> ", karesi(4));
+
+inc(4);
+inc(6);
+dec();
+console.log("dec() :>> ", dec());
+inc();
+console.log("counter :>> ", counter);
+// counter = 29; // ! sabit gibi davranır
+inc();
+console.log("counter :>> ", counter);
+selam();
+ugurla();
+>>>>>>> cf16bf3420f42f821e134cd3bbe6d547b77f0181
