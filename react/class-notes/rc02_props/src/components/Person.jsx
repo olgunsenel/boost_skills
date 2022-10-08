731a38a69abb485e1996570d53452b364cf32fd4
felix <felixfranko66@gmail.com>
2022-10-08 13:56:09 +0300

    v13

--- react/class-notes/rc02_props/src/components/Person.jsx
+++ react/class-notes/rc02_props/src/components/Person.jsx
@@ -1,12 +1,12 @@
-const Person = () => {
+import Msg from "./Msg";
+
+const Person = (props) => {
+  const { name, img, tel } = props;
   return (
-    <div>
-      <p>Merhaba Felix</p>
-      <img
-        src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
-        alt="img1"
-      />
-      <p>55555555</p>
+    <div style={{ textAlign: "center" }}>
+      <Msg name={name} />
+      <img style={{ width: "200px" }} src={img} alt="img1" />
+      <p>{tel}</p>
     </div>
   );
 };
