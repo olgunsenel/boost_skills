caa0eb716bcb082751651b4c14d13b20d853fb8e
felix <felixfranko66@gmail.com>
2022-10-08 13:42:17 +0300

    v11

--- react/class-notes/rc02_props/src/App.js
+++ react/class-notes/rc02_props/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Msg from "./components/Msg";
+import Person from "./components/Person";
 
 function App() {
   return (
     <div>
-      <Msg name="Felix" />
+      <Person />
     </div>
   );
 }
---
+++ react/class-notes/rc02_props/src/components/Person.jsx
@@ -0,0 +1,14 @@
+const Person = () => {
+  return (
+    <div>
+      <p>Merhaba Felix</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2017/08/30/12/45/girl-2696947__480.jpg"
+        alt="img1"
+      />
+      <p>55555555</p>
+    </div>
+  );
+};
+
+export default Person;
