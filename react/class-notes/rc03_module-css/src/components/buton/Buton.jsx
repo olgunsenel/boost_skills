cfaff15b285bcb6fea0a408984e037b7ed87777c
profnoah <noah@clarusway.com>
2022-10-10 21:53:12 +0300

    title

--- react/class-notes/rc03_module-css/src/components/buton/Buton.jsx
+++ react/class-notes/rc03_module-css/src/components/buton/Buton.jsx
@@ -1,10 +1,11 @@
-import "./Buton.css";
+// import "./Buton.css";
+import ButonStyle from "./buton.module.css";
 
 const Buton = ({ btn }) => {
   console.log(btn);
   return (
-    <div>
-      <button>{btn}</button>
+    <div className={ButonStyle.title}>
+      <button className={ButonStyle["btn-blue"]}>{btn}</button>
     </div>
   );
 };
