4168c2727c46bb240351da6d87ab830da9234c54
profnoah <noah@clarusway.com>
2022-10-10 23:21:56 +0300

    card

--- react/class-notes/rc03_sass/src/components/Card.jsx
+++ react/class-notes/rc03_sass/src/components/Card.jsx
@@ -7,14 +7,14 @@ const Card = ({ data }) => {
       {data.map((item) => {
         const { id, name, job, comment, img } = item;
         return (
-          <div key={id}>
+          <div key={id} className={CardStyle.card}>
             <h1>{name}</h1>
             <h2>{job}</h2>
             <p>{comment}</p>
             <img src={img} alt="img" />
-            <div>
-              <button>Small</button>
-              <button>Large</button>
+            <div className={CardStyle.buttons}>
+              <button className={CardStyle["buttons--small"]}>Small</button>
+              <button className={CardStyle["buttons--large"]}>Large</button>
             </div>
           </div>
         );
