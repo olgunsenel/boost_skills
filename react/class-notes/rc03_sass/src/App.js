d1c717011e35977708d26279bcc72fc2fe064fd5
profnoah <noah@clarusway.com>
2022-10-10 23:06:14 +0300

    sass

--- react/class-notes/rc03_sass/src/App.js
+++ react/class-notes/rc03_sass/src/App.js
@@ -3,11 +3,13 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import "./scss/App.scss";
 
+import data from "./data";
+
 function App() {
   return (
     <>
       <Header />
-      <Card />
+      <Card data={data} />
       <Footer />
     </>
   );
--- react/class-notes/rc03_sass/src/components/Card.jsx
+++ react/class-notes/rc03_sass/src/components/Card.jsx
@@ -1,5 +1,26 @@
-const Card = () => {
-  return <h1>Card</h1>;
+import CardStyle from "../scss/card.module.scss";
+
+const Card = ({ data }) => {
+  console.log(data);
+  return (
+    <div className={CardStyle.container}>
+      {data.map((item) => {
+        const { id, name, job, comment, img } = item;
+        return (
+          <div key={id}>
+            <h1>{name}</h1>
+            <h2>{job}</h2>
+            <p>{comment}</p>
+            <img src={img} alt="img" />
+            <div>
+              <button>Small</button>
+              <button>Large</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Card;
